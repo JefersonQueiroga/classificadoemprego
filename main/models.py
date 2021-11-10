a83ad4d47e46d28c5c74ac14d9263db25fcf5a2e
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2021-11-10 20:41:00 -0300

    Adicionando novos campos e imagens

--- main/models.py
+++ main/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class Cidade (models.Model):
     nome = models.CharField(max_length=150)
     sigla_estado = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.nome
 
 class Empresa (models.Model):
     nome = models.CharField(max_length=150,blank=False)
     cnpj = models.CharField(max_length=15,blank=False)
     telefone = models.CharField(max_length=15,blank=True)
     endereco =models.CharField(max_length=150,blank=True)
-    cidade = models.ForeignKey(Cidade,on_delete=models.CASCADE) 
+    cidade = models.ForeignKey(Cidade,on_delete=models.CASCADE)
+    image = models.FileField(upload_to='imagens/',blank=True)
 
 
 class TipoVaga (models.Model):
     nome = models.CharField(max_length=150)
-   
+    
+    def __str__(self):
+        return self.nome
 
 class Vaga(models.Model):
     descricao = models.TextField()
@@ -26,3 +34,7 @@ class Vaga(models.Model):
     empresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)
     tipo_vaga = models.ForeignKey(TipoVaga,on_delete=models.CASCADE)
     usuarios = models.ManyToManyField(User)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.descricao
