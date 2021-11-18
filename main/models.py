fe038597075cd8cd4e79c09fa8a796e44823d2e0
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2021-11-18 12:33:03 -0300

    acidionano aprimoramento model

--- main/models.py
+++ main/models.py
@@ -3,18 +3,33 @@ from django.contrib.auth.models import User
 class Cidade (models.Model):
     nome = models.CharField(max_length=150)
     sigla_estado = models.CharField(max_length=2)
+    foto = models.FileField(upload_to='cidades/',blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        ordering = ['nome'] #Sort in desc order
+
 
 class Empresa (models.Model):
     nome = models.CharField(max_length=150,blank=False)
-    cnpj = models.CharField(max_length=15,blank=False)
+    cnpj = models.CharField(max_length=20,blank=False)
     telefone = models.CharField(max_length=15,blank=True)
     endereco =models.CharField(max_length=150,blank=True)
     cidade = models.ForeignKey(Cidade,on_delete=models.CASCADE) 
+    email = models.EmailField(blank=False)
+
+    def __str__(self):
+        return self.nome
+
 
 
 class TipoVaga (models.Model):
     nome = models.CharField(max_length=150)
-   
+    
+    def __str__(self):
+        return self.nome
 
 class Vaga(models.Model):
     descricao = models.TextField()
@@ -26,3 +41,6 @@ class Vaga(models.Model):
     empresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)
     tipo_vaga = models.ForeignKey(TipoVaga,on_delete=models.CASCADE)
     usuarios = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return self.descricao
