58a934e20d218ec55a90313df68c2e6c26254cf3
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2021-11-25 16:50:59 -0300

    Adicionar template bootstrap.

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
    path('',index,name='index'),
    path('listar_cidades/',listagem_cidades,name='listar_cidades'),
    path('detalhe_cidade/<int:id_cidade>/', detalhe, name='detalhe_cidade'),
+   path('quem_somos/', quem_somos ,name='quem_somos'),
 ] 
--- main/views.py
+++ main/views.py
@@ -4,15 +4,18 @@ from .models import Cidade
 
 def index(request):
     lista = Cidade.objects.all()
-    return render(request,'main/index.html', context={ "cidades": lista})
+    return render(request,'index.html', context={ "cidades": lista})
 
 def acai_bom(request):
     return HttpResponse("Açai 500g - Leite Condensaddo - Granola - Nutella")
 
 def listagem_cidades( request ):
     lista = Cidade.objects.all()
-    return render(request,'main/listar_cidades.html', context={ "cidades" : lista  })    
+    return render(request,'listar_cidades.html', context={ "cidades" : lista  })    
 
 def detalhe( request, id_cidade  ):
     cidade = Cidade.objects.get(id= id_cidade )
-    return render(request, 'main/detalhe.html', context={'cidade':cidade})    
+    return render(request, 'main/detalhe.html', context={'cidade':cidade})  
+
+def quem_somos(request):
+    return render(request,'quem_somos.html')  
